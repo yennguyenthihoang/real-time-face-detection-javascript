0c9829f71f4f24b79766e86a9170e35c92a4c79b
yennguyenthihoangvn <yennguyenthihoangvn@gmail.com>
2019-07-03 23:45:20 +0200

    initial project

---
+++ public/js/commons.js
@@ -0,0 +1,46 @@
+async function requestExternalImage(imageUrl) {
+  const res = await fetch('fetch_external_image', {
+    method: 'post',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({ imageUrl })
+  })
+  if (!(res.status < 400)) {
+    console.error(res.status + ' : ' + await res.text())
+    throw new Error('failed to fetch image from url: ' + imageUrl)
+  }
+
+  let blob
+  try {
+    blob = await res.blob()
+    return await faceapi.bufferToImage(blob)
+  } catch (e) {
+    console.error('received blob:', blob)
+    console.error('error:', e)
+    throw new Error('failed to load image from url: ' + imageUrl)
+  }
+}
+
+function renderNavBar(navbarId, exampleUri) {
+  const examples = [
+  ]
+  const navbar = $(navbarId).get(0)
+  const pageContainer = $('.page-container').get(0)
+}
+
+function renderSelectList(selectListId, onChange, initialValue, renderChildren) {
+  const select = document.createElement('select')
+  $(selectListId).get(0).appendChild(select)
+  renderChildren(select)
+  $(select).val(initialValue)
+  $(select).on('change', (e) => onChange(e.target.value))
+  $(select).material_select()
+}
+
+function renderOption(parent, text, value) {
+  const option = document.createElement('option')
+  option.innerHTML = text
+  option.value = value
+  parent.appendChild(option)
+}
